35a6feee62981f9d0cfe23614c0f457c1e81a5a9
camistrika <128085746+camistrika@users.noreply.github.com>
2024-05-21 15:33:20 -0300

    Update ArbolID3.py

--- ArbolID3.py
+++ ArbolID3.py
@@ -11,8 +11,17 @@ class ArbolID3(Arbol):
         self.es_hoja = es_hoja
         self.hijos = {}
 
+    
+    
     @classmethod
-    def id3(cls, X: np.ndarray, y: np.ndarray, atributos: List[int], profundidad_max: None, minimas_obs_n: int = 0, ganancia_minima: int= 0, profundidad_actual: int = 0) -> "ArbolID3":
+    def id3(cls, X: np.ndarray, y: np.ndarray, atributos: List[int], profundidad_max: None, minimas_obs_n: int = 0, minimas_obs_h: int = 0, ganancia_minima: int = 0, profundidad_actual: int = 0) -> "ArbolID3":
+
+        # Criterio de parada: Sin atributos para dividir
+        if not atributos:
+            # Devolver la clase mayoritaria
+            clase_mayoritaria = cls.clase_mayoritaria(y)
+            return ArbolID3(clase_mayoritaria, es_hoja=True)
+        
         # Criterio de parada: Nodo puro (todos los elementos del nodo pertenecen a la misma clase)
         if len(np.unique(y)) == 1:
             return ArbolID3(y[0], es_hoja = True)
@@ -22,17 +31,13 @@ class ArbolID3(Arbol):
             clase_mayoritaria= cls.clase_mayoritaria(y)
             return ArbolID3(clase_mayoritaria, es_hoja = True)
         
-        #criterio de parada: Mininimas observaciones por nodo(Que diferencia hay con minimas observaciones por hoja???)
+        #criterio de parada: Mininimas observaciones por nodo
         if minimas_obs_n > len(y):
             clase_mayoritaria= cls.clase_mayoritaria(y)
             return ArbolID3(clase_mayoritaria, es_hoja = True)
         
         
-        # Criterio de parada: Sin atributos para dividir
-        if not atributos:
-            # Devolver la clase mayoritaria
-            clase_mayoritaria = cls.clase_mayoritaria(y)
-            return ArbolID3(clase_mayoritaria, es_hoja=True)
+        
         
         
         
@@ -41,7 +46,7 @@ class ArbolID3(Arbol):
         mejor_atributo = atributos[np.argmax(ganancias)]
         
         #criterio de parada: ganancia minima
-        if ganancias[np.argmax(ganancias)]< ganancia_minima:
+        if ganancias[np.argmax(ganancias)] < ganancia_minima:
             clase_mayoritaria = cls.clase_mayoritaria(y)
             return ArbolID3(clase_mayoritaria, es_hoja=True)
         
@@ -59,14 +64,22 @@ class ArbolID3(Arbol):
             indices = np.where(X[:, mejor_atributo] == valor)[0]
             sub_X = X[indices]
             sub_y = y[indices]
+
+            # Criterio de parada: Mínimas observaciones por hoja
+            if len(sub_y) < minimas_obs_h:
+                clase_mayoritaria = cls.clase_mayoritaria(sub_y)
+                subarbol = ArbolID3(valor=clase_mayoritaria, es_hoja=True)
+            else:
+                # Recursión para construir el subárbol
+                subarbol = cls.id3(sub_X, sub_y, atributos_restantes, profundidad_max, minimas_obs_n, minimas_obs_h, ganancia_minima, profundidad_actual + 1)
             
-            # Recursión para construir el subárbol
-            subarbol = cls.id3(sub_X, sub_y, atributos_restantes, profundidad_max, minimas_obs_n, ganancia_minima, profundidad_actual + 1)
             arbol.hijos[valor] = subarbol
         
-        
         return arbol
-            
+        
+    
+    
+    
     @staticmethod
     def csv_a_numpy(ruta_csv: str) -> tuple:
         df = pd.read_csv(ruta_csv)
@@ -87,7 +100,7 @@ class ArbolID3(Arbol):
     def entropia(y: np.ndarray) -> float:
         clases_por_atributo, cantidad_cada_clase = np.unique(y, return_counts=True)
         probabilidad_por_clase = cantidad_cada_clase / len(y)
-        return -np.sum(probabilidad_por_clase * np.log2(probabilidad_por_clase + 1e-9))  # Agregar una pequeña constante para evitar log(0)
+        return -np.sum(probabilidad_por_clase * np.log2(probabilidad_por_clase))  
 
     @staticmethod
     def ganancia_informacion_atributo(X: np.ndarray, y: np.ndarray, indice_atributo: int) -> float:
@@ -100,7 +113,9 @@ class ArbolID3(Arbol):
             entropia_ponderada += (len(subconjunto_clase) / len(y)) * ArbolID3.entropia(subconjunto_clase)
         
         return entropia_inicial - entropia_ponderada
-
+        
+        
+        
         
         
         
